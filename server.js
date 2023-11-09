50071d839bb2e6ab80a305736a121e60d19baa96
Stefano <stefe68@gmail.com>
2023-11-09 17:00:28 +0100

    Add reading ang writing jokes in json file

--- server.js
+++ server.js
@@ -14,12 +14,53 @@ dotenv.config();
  */
 let port = +process.env.PORT || 3000;
 
+/* define file path */
+const filePath = path.join(__dirname, 'norrisDb.json');
+
+
+/* read file and save joke */
+function saveJoke(joke)
+{
+	fs.readFile(filePath, 'utf8', (err, data) =>
+	{
+		let jokes = [];
+
+		if (!err && data)
+		{
+			try
+			{
+				jokes = JSON.parse(data);
+				if (!Array.isArray(jokes))
+				{
+					jokes = [];
+				}
+			} catch (e)
+			{
+				jokes = [];
+			}
+		}
+
+		jokes.push(joke);
+
+		fs.writeFile(filePath, JSON.stringify(jokes, null, 2), (err) =>
+		{
+			if (err)
+			{
+				console.error('Error writing joke to file:', err);
+			}
+		});
+	});
+}
+
+
 /* create server */
 const server = http.createServer((req, res) =>
 {
 	loadAjaxData((data) =>
 	{
 		const joke = data.value;
+
+		saveJoke(joke);
 
 		res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 		res.end(`<h1>${joke}</h1>`);
